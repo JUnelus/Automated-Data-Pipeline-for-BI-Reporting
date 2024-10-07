c633db5995e795c3d9d099d46c8b63606a78e740
Jimmy Unelus <jimmyunelus@gmail.com>
2024-10-06 23:37:34 -0400

    add requirements.txt file and update loading into postgres

--- scripts/load.py
+++ scripts/load.py
@@ -1,21 +1,20 @@
+import os
 import sqlalchemy as db
 import pandas as pd
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 
-def load_data_to_redshift(file_path, redshift_config):
+def load_data_to_postgres(file_path):
     engine = db.create_engine(
-        f"postgresql://{redshift_config['redshift_user']}:{redshift_config['redshift_password']}@{redshift_config['redshift_host']}/{redshift_config['redshift_db']}")
+        f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}")
     df = pd.read_csv(file_path)
 
     with engine.connect() as conn:
-        df.to_sql('financial_data', conn, if_exists='replace', index=False)
+        df.to_sql('apple_financial_data', conn, if_exists='replace', index=False)
 
 
 if __name__ == '__main__':
-    config = {
-        'redshift_user': 'admin',
-        'redshift_password': 'LSKXWjkefl834&(',
-        'redshift_host': 'redshift-cluster-endpoint',
-        'redshift_db': 'mydb'
-    }
-    load_data_to_redshift('transformed_data.csv', config)
+    load_data_to_postgres('C:/Users/big_j/PycharmProjects/Automated-Data-Pipeline-for-BI-Reporting/scripts/aapl_stock_data_transformed.csv')
